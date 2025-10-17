d9217c4e15e9f10356b3a1930c1d477bd2595803
orokidd <gregomily@gmail.com>
2025-10-17 15:48:31 +0800

    added more request on router

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -7,5 +7,10 @@ bookRouter.get("/:bookId", (req, res) => {
   const { bookId } = req.params;
   res.send(`Book ID: ${bookId}`);
 });
+bookRouter.post("/", (req, res) => res.send("Create a new book"));
+bookRouter.put("/:bookId", (req, res) => {
+  const { bookId } = req.params;
+  res.send(`Edited a book with ID: ${bookId}`);
+});
 
 module.exports = bookRouter;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,7 @@ const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => res.send("Welcome to the Bookstore API"));
 indexRouter.get("/about", (req, res) => res.send("About this API"));
+indexRouter.get("/contact", (req, res) => res.send("Contact us at contact@bookstore.com"));
+indexRouter.post("/contact", (req, res) => res.send("Contact form submitted"));
 
 module.exports = indexRouter;
