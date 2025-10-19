0e1bd4fb71ceb136466bf438b73d70f7d4ddad7b
orokidd <gregomily@gmail.com>
2025-10-19 14:02:33 +0800

    server side render with ejs

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+  { href: "authors", text: "Authors" },
+  { href: "books", text: "Books" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { links: links });
+});
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
