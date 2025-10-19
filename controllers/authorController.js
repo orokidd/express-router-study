43ccd540f425321533516f5acff86f3e5dd54796
orokidd <gregomily@gmail.com>
2025-10-19 12:41:06 +0800

    added custom error

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -8,8 +8,7 @@ async function getAuthorById(req, res) {
   const author = await db.getAuthorById(Number(authorId));
 
   if (!author) {
-    res.status(404).send("Author not found");
-    return;
+    throw new CustomNotFoundError("Author not found");
   }
 
   res.send(`Author Name: ${author.name}`);
